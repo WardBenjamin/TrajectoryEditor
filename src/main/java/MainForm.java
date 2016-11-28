096e4ee7dfe1f5f177016fb2e6f8809350229e1b
ward.programm3r@gmail.com <ward.programm3r@gmail.com>
2016-11-28 17:54:34 -0500

    Add settings panel, force path viewer to use less samples and larger deltatime for faster path generation when only viewing

--- src/main/java/MainForm.java
+++ src/main/java/MainForm.java
@@ -5,6 +5,7 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.DomainInfo;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
@@ -13,6 +14,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.nio.file.Path;
 import java.util.ArrayList;
 
 /**
@@ -32,7 +34,21 @@ public class MainForm {
     private JButton button_reset;
     private JButton button_view;
     private JButton button_export;
+    private JLabel label_maxVel;
+    private JTextField textField_maxVel;
+    private JLabel label_maxAccl;
+    private JTextField textField_maxAccl;
+    private JLabel label_maxJerk;
+    private JTextField textField_maxJerk;
+    private JLabel label_deltaTime;
+    private JTextField textField_deltaTimeHZ;
+    private JLabel label_maxVel_unit;
+    private JLabel label_maxAccl_unit;
+    private JLabel label_maxJerk_unit;
+    private JLabel label_deltaTime_unit;
+    private JButton button_saveSettings;
 
+    private double max_vel, max_accl, max_jerk, deltaTime;
     private ArrayList<Waypoint> waypoints = new ArrayList<>();
     private ChartFrame chartFrame;
 
@@ -61,6 +77,18 @@ public class MainForm {
                 MainForm.this.action_export(e);
             }
         });
+        button_saveSettings.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainForm.this.action_settings(e);
+            }
+        });
+
+        // Set variables with default values
+        max_vel = Double.valueOf(this.textField_maxVel.getText());
+        max_accl = Double.valueOf(this.textField_maxAccl.getText());
+        max_jerk = Double.valueOf(this.textField_maxJerk.getText());
+        deltaTime = 1.0 / Double.valueOf(this.textField_deltaTimeHZ.getText());
     }
 
     private void createUIComponents() {
@@ -96,7 +124,7 @@ public class MainForm {
     }
 
     private void action_graph(ActionEvent evt) {
-        Trajectory trajectory = finalizeTrajectory();
+        Trajectory trajectory = finalizeTrajectory(CalculationSpeed.FAST);
 
         if (trajectory == null) {
             return;
@@ -108,11 +136,15 @@ public class MainForm {
         for (int i = 0; i < trajectory.segments.length; i++) {
             Trajectory.Segment segment = trajectory.segments[i];
             series.add(segment.x, segment.y);
-            System.out.println("X, Y: " + segment.x + ", " + segment.y);
+            //System.out.println("X, Y: " + segment.x + ", " + segment.y);
         }
 
         collection.addSeries(series);
 
+        if(chartFrame != null) {
+            chartFrame.dispose();
+            //chartFrame.setVisible(false);
+        }
         JFreeChart chart = ChartFactory.createScatterPlot("Robot", "X", "Y",
                 collection, PlotOrientation.VERTICAL, true, true, false);
         chartFrame = new ChartFrame("Robot", chart);
@@ -121,24 +153,59 @@ public class MainForm {
     }
 
     private void action_export(ActionEvent evt) {
-        Trajectory trajectory = finalizeTrajectory();
+        Trajectory trajectory = finalizeTrajectory(CalculationSpeed.SLOW);
         String path = "." + File.separator + "output.csv";
         System.out.println("Exporting trajectory to: " + path);
         Pathfinder.writeToCSV(new File(path), trajectory);
     }
 
-    private Trajectory finalizeTrajectory() {
-        double max_velocity = 1.7, max_acceleration = 2.0, max_jerk = 60.0;
-        Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_QUINTIC,
-                Trajectory.Config.SAMPLES_HIGH, 0.05, max_velocity, max_acceleration, max_jerk); // Delta time 0.05 = 1/20
+    private void action_settings(ActionEvent evt) {
+        boolean failed = false;
+        try { max_vel = Double.valueOf(this.textField_maxVel.getText()); }
+        catch(Exception e) { System.out.println("Maximum velocity invalid."); failed = true; }
+
+        try { max_accl = Double.valueOf(this.textField_maxAccl.getText()); }
+        catch(Exception e) { System.out.println("Maximum acceleration invalid."); failed = true; }
+
+        try { max_jerk = Double.valueOf(this.textField_maxJerk.getText()); }
+        catch(Exception e) { System.out.println("Maximum jerk invalid."); failed = true; }
+
+        try { deltaTime = 1.0 / Double.valueOf(this.textField_deltaTimeHZ.getText()); } // Converts hz to seconds
+        catch(Exception e) { System.out.println("Delta time invalid."); failed = true; }
+
+        if(!failed)
+            System.out.println("Settings updated successfully!");
+        else
+            System.out.println("Settings not updated successfully; check above for options with invalid input.");
+    }
+
+    enum CalculationSpeed
+    {
+        SLOW,
+        FAST
+    }
+
+    private Trajectory finalizeTrajectory(CalculationSpeed calcSpeed) {
+        Trajectory.Config config;
+        if(calcSpeed == CalculationSpeed.FAST)
+            config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_QUINTIC, Trajectory.Config.SAMPLES_HIGH, deltaTime, max_vel, max_accl, max_jerk);
+        else
+            config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_QUINTIC, Trajectory.Config.SAMPLES_LOW, deltaTime * 3, max_vel, max_accl, max_jerk);
         Waypoint[] array = new Waypoint[waypoints.size()];
+
+        Trajectory trajectory = null;
 
         if (waypoints.size() > 0) {
             System.out.println("Finalized trajectory with " + array.length + " waypoints!");
-            return Pathfinder.generate(waypoints.toArray(array), config);
+            try {
+                trajectory = Pathfinder.generate(waypoints.toArray(array), config);
+            }
+            catch(Exception e) {
+                System.out.println("Failed to finalize trajectory because settings are invalid or path not able to be constructed!");
+            }
         }
+        else System.out.println("Failed to finalize trajectory because it had zero elements!");
 
-        System.out.println("Failed to finalize trajectory because it had zero elements!");
-        return null;
+        return trajectory;
     }
 }
